d7a26d1a15ce7d8c6f86ec2f3456337ef4d75699
RubenPiedrahitaM <105086007+RubenPiedrahitaM@users.noreply.github.com>
2022-11-06 12:52:43 -0500

    Update

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.get('/', async (req, res) => {
   app.get('/Productos/:Id_Producto', async (req, res) => {
     const id = req.params
     const [rows] = await pool.query('SELECT * FROM Productos where ?',[id])
-    res.json(rows)
+    res.json(rows[0])
   })
 
   app.listen(PORT)
