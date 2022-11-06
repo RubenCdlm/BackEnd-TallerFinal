f6e03677564a4e738ae4539bebeb0ae46f8f8c8d
RubenPiedrahitaM <105086007+RubenPiedrahitaM@users.noreply.github.com>
2022-11-06 13:53:17 -0500

    Update

--- src/app.js
+++ src/app.js
@@ -23,18 +23,14 @@ app.get('/', async (req, res) => {
     const id = req.params
     const rows = [];
     console.log(id);
-    if(id != ""){
       rows = await pool.query('SELECT * FROM Productos where ?',[id])
       res.json(rows[0])
-    }else{
-      rows = await pool.query('SELECT * FROM Productos')
-      res.json(rows)
-    }
+    
   })
-  app.get('/InsertClient', async (req, res) => {
+  app.get('/Productos', async (req, res) => {
     const id = req.params
-    const [rows] = await pool.query('SELECT * FROM Productos where ?',[id])
-    res.json(rows[0])
+    const [rows] = await pool.query('SELECT * FROM Productos')
+    res.json(rows)
   })
 
   app.listen(PORT)
