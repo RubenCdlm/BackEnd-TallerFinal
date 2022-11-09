ef7e5e7803e7065ad24caf28b9158ab617a3b0fb
RubenPiedrahitaM <105086007+RubenPiedrahitaM@users.noreply.github.com>
2022-11-09 15:00:05 -0500

    Update

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import  fs  from 'fs'
 const app = express();
 
 app.use(express.json());
-app.use(express.static(__dirname + '/src/Imagenes'));
+app.use(express.static('/src/Imagenes'));
 
 
   app.get('/cliente', async (req, res) => {
